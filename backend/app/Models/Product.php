8a2db2cdd85bc9ff976f97f955a1599252b70a29
Dušan Milekić <dusanmilekic0511@gmail.com>
2025-12-22 13:56:43 +0100

    Starting project..

---
+++ backend/app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'stock_quantity',
+        'category',
+        'likes',
+        'image_url',
+    ];
+    protected $casts = [
+        'price' => 'decimal:2',
+        'stock_quantity' => 'integer',
+        'likes' => 'integer',
+    ];
+    
+}
